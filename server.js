d01bbd978dc651c5e689d93dff7fa75d2525ffc0
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-08-06 01:42:54 -0500

    changed session time limit to one hour

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const sess = {
   secret: 'Super secret secret',
   cookie: {
     // Stored in milliseconds
-    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    maxAge:  60 * 60 * 1000, // expires after 1 hour
   },
   resave: false,
   saveUninitialized: true,
