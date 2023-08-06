474455d40a7a6a42d18fc9f9cfb08a658caeeae9
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-08-06 01:26:23 -0500

    cleaned up the files to remove gallery and painting code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Gallery, Painting, Post, Comment } = require('../models');
 
-// GET all galleries for homepage
+// GET all posts for homepage
 router.get('/', async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
@@ -25,46 +25,6 @@ router.get('/', async (req, res) => {
       posts,
       loggedIn: req.session.loggedIn,
     });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// GET one gallery
-router.get('/gallery/:id', async (req, res) => {
-  try {
-    const dbGalleryData = await Gallery.findByPk(req.params.id, {
-      include: [
-        {
-          model: Painting,
-          attributes: [
-            'id',
-            'title',
-            'artist',
-            'exhibition_date',
-            'filename',
-            'description',
-          ],
-        },
-      ],
-    });
-
-    const gallery = dbGalleryData.get({ plain: true });
-    res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// GET one painting
-router.get('/painting/:id', async (req, res) => {
-  try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
-
-    const painting = dbPaintingData.get({ plain: true });
-    res.render('painting', { painting, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
--- models/index.js
+++ models/index.js
@@ -1,16 +1,6 @@
 const User = require('./User');
-const Gallery = require('./Gallery');
-const Painting = require('./Painting');
 const Post = require('./Post');
 const Comment = require('./Comment');
-
-Gallery.hasMany(Painting, {
-  foreignKey: 'gallery_id',
-});
-
-Painting.belongsTo(Gallery, {
-  foreignKey: 'gallery_id',
-});
 
 Post.hasMany(Comment, {
   foreignKey: 'post_id'
@@ -20,4 +10,4 @@ Comment.belongsTo(Post, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Gallery, Painting, Post, Comment };
+module.exports = { User, Post, Comment };
